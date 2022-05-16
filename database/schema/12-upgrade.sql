314e2189a01b52d5767f02a8601b06f3edaa0a52
Magic Cat <pusz.monika@gmail.com>
2022-05-16 14:14:28 +0100

    updated upgrade_params table

--- database/schema/12-upgrade.sql
+++ database/schema/12-upgrade.sql
@@ -1,7 +1,7 @@
 CREATE TABLE upgrade_params
 (
     one_row_id      BOOLEAN   NOT NULL DEFAULT TRUE PRIMARY KEY,
-    binary_version  JSONB     NOT NULL,
+    binary_version  TEXT     NOT NULL,
     upgrade_info    TEXT      NOT NULL,
     upgrade_height  BIGINT    NOT NULL,
     upgrade_time    TIMESTAMP NOT NULL,
